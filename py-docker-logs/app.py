0de771c9f834b1c0334589ee4e3a97e7dd28ced1
Victoryusz <hugo4strong@gmail.com>
2025-03-03 18:00:48 -0300

    Codigo em python que simula Logs Ficticios.

--- py-docker-logs/app.py
+++ py-docker-logs/app.py
@@ -1,12 +1,23 @@
-import time
+import time 
+import random
 
 def main():
     count = 0
-    while True:
-        print(f"Contador: {count}")
-        count += 1
-        time.sleep(1)
+    log_levels = ["INFO", "DEBUG", "ERROR","PERIGO"]
+    
+    with open("app.log", "a") as log_file:
+        while True:
+            # Escolhendo um nível de log aleatório
+            log_level = random.choice(log_levels)
+            
+            # Msg fictícias para logs
+            log_message = f"[{log_level}] - Contador: {count} - Log Gerado em: {time.strftime('%Y-%m-%d %H:%M:%S')}\n" 
+
+            # mostra os logs no console e escreve no arquivo
+            print(log_message, end='') # mostra no console
+            log_file.write(log_message) # escreve no arquivo
+            
+            count += 1
+            time.sleep(1)
 if __name__ == "__main__":
     main()
-    
-# Este código cria um contador simples que imprime o valor na tela a cada SEGUNDO.
